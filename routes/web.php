0bf29a690e00d92420f59ee29ef34cc45ee271cf
voalisher <135119976+voalisher@users.noreply.github.com>
2023-05-31 15:31:12 +0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/team', function () {
+    return view('team');
+});
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+Route::get('/class', function () {
+    return view('class');
+});
